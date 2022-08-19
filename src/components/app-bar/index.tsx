5b4e84a0abb6b0f88db6c233dbf45052d65e0048
Demid Mosko <demid.mosko@gmail.com>
2022-08-19 18:04:19 +0400

    Add metamask and more chains

--- src/components/app-bar/index.tsx
+++ src/components/app-bar/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { AppBar as MuiAppBar, CircularProgress, Toolbar } from '@mui/material';
+import { AppBar as MuiAppBar, Button, CircularProgress, Toolbar } from '@mui/material';
 import { State } from './state';
 import { ChainSelector } from './chain-selector';
+import { useChainCtx } from 'contexts/web3';
 
 export const AppBar: React.FC = () => {
+  const ctx = useChainCtx();
   return (
     <MuiAppBar
       position="static"
@@ -13,6 +15,13 @@ export const AppBar: React.FC = () => {
       <Toolbar>
         <State />
         <ChainSelector />
+        <Button
+          onClick={() => {
+            ctx.setConnector('metamask');
+          }}
+        >
+          Connect MetaMask
+        </Button>
       </Toolbar>
     </MuiAppBar>
   )
--- src/contexts/web3/web3.contexts.tsx
+++ src/contexts/web3/web3.contexts.tsx
@@ -1,7 +1,7 @@
 import Web3 from 'web3';
 import Web3Provider from 'web3-react';
 import React, { useEffect, useMemo } from 'react';
-import { Connector, NetworkOnlyConnector } from 'web3-react/dist/connectors';
+import { Connector, InjectedConnector, NetworkOnlyConnector } from 'web3-react/dist/connectors';
 import { useChainList } from 'hooks/use-chain-list';
 import { useSearchParams } from 'react-router-dom';
 import { useChainCtx } from './web3.hooks';
@@ -48,13 +48,15 @@ export const ChainCtxProvider: React.FC<iProps> = (props) => {
   const { chainList } = useChainList();
 
   const connectors = useMemo(() => {
-    const connectors = chainList.reduce<Record<string, Connector>>((map, network) => {
+    // TODO: any to connector
+    const connectors = chainList.reduce<Record<string, any>>((map, network) => {
       for (const rpc of network.rpc) {
         const connector = new NetworkOnlyConnector({ providerURL: rpc });
         map[rpc] = connector;
       }
       return map;
     }, {});
+    connectors.metamask = new InjectedConnector();
     return connectors;
   }, [chainList]);
 
