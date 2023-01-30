28b3fe61e23e275263f4dbc05eb790cd20c6e938
djomaa <demid.mosko@gmail.com>
2023-01-30 22:16:19 +0800

    Add infinity load to chains

--- src/components/chain-manager/chain-manager.tsx
+++ src/components/chain-manager/chain-manager.tsx
@@ -1,4 +1,5 @@
-import React, { useMemo } from 'react'
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
@@ -8,25 +9,49 @@ import Typography from '@mui/material/Typography'
 
 import { useLogger } from 'hooks/use-logger'
 import { searchChain, useChainList } from 'hooks/use-chain-list/use-chain-list'
+// import LazilyRender from 'react-lazily-render';
 
-import { ChainCard } from './chain-card'
+import { ChainCard } from './chain.card'
 import { useSearch } from 'hooks/use-search'
 import Link from '@mui/material/Link'
+import { Chain } from 'types/chain';
+// import Skeleton from '@mui/material/Skeleton'
 
+const PageSize = 6;
 export const ChainManager: React.FC = () => {
   const [Logger] = useLogger(ChainManager.name)
   const { chainList } = useChainList()
   const [search, setSearch, searchList] = useSearch(chainList, searchChain)
 
+  const page = useRef(0);
+  const [items, setItems] = useState<Chain[]>([])
+
+  useEffect(() => {
+    page.current = 2;
+    setItems(searchList.slice(0, PageSize * 2));
+    Logger.debug('Refresh', { page: page.current, newItem: searchList.slice(0, PageSize), items });
+  }, [searchList])
+
   const list = useMemo(() => {
-    return searchList.map((chain) => {
+    return items.map((chain) => {
       return (
         <Grid key={chain.chainId} xs={12} sm={4}>
           <ChainCard key={chain.chainId} {...chain} />
         </Grid>
       )
     })
-  }, [searchList]);
+  }, [items]);
+
+  const handleNext = useCallback(() => {
+    page.current += 1;
+    const from = PageSize * page.current;
+    const to = Math.min(from + PageSize, searchList.length);
+    const newItems = searchList.slice(from, to);
+    // setTimeout(() => { setItems((prev) => [...prev, ...newItems]); }, 3e3);
+    setItems((prev) => [...prev, ...newItems]);
+    Logger.debug('Handle next', { from, to, newItems })
+  }, []);
+
 
   return (
     <Box>
@@ -57,9 +82,18 @@ export const ChainManager: React.FC = () => {
           value={search}
           onChange={(e) => { setSearch(e.target.value) }}
         />
-        <Grid container spacing={2}>
-          {list}
-        </Grid>
+        <InfiniteScroll
+          scrollThreshold={0.2}
+          dataLength={items.length}
+          next={handleNext}
+          hasMore={items.length < searchList.length}
+          loader={<></>}
+        // loader={<Skeleton width='100%' height='100px' variant='text' />}
+        >
+          <Grid container spacing={2}>
+            {list}
+          </Grid>
+        </InfiniteScroll>
       </Stack>
     </Box >
   )
--- src/components/chain-manager/chain.card.tsx
+++ src/components/chain-manager/chain.card.tsx
@@ -1,23 +1,24 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import { Link } from 'react-router-dom'
 
 import Card from '@mui/material/Card'
 import Stack from '@mui/material/Stack'
+import MuiLink from '@mui/material/Link'
 import Button from '@mui/material/Button'
 import Avatar from '@mui/material/Avatar'
 import Tooltip from '@mui/material/Tooltip'
+import Grid from '@mui/system/Unstable_Grid'
 import CardHeader from '@mui/material/CardHeader'
 import Typography from '@mui/material/Typography'
 import CardContent from '@mui/material/CardContent'
 import CardActions from '@mui/material/CardActions'
-import MuiLink from '@mui/material/Link'
 
 import { Chain } from 'types/chain'
 import { createRoute, Route } from 'constants/route'
-import Grid from '@mui/system/Unstable_Grid'
-import { Link } from 'react-router-dom'
+import { getWebsiteFavicon } from 'utils/google'
 
 const CustomAvatar: React.FC<Chain> = (chain) => {
-  const url = `https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${chain.infoURL}&size=64`
+  const url = useMemo(() => getWebsiteFavicon(chain.infoURL), [chain]);
   return (
     <Avatar
       src={url}
@@ -32,7 +33,7 @@ export const ChainCard: React.FC<Chain> = (chain: Chain) => {
     <Card>
       <CardHeader
         title={chain.name}
-        subheader={chain.shortName}
+        subheader={<MuiLink href={chain.infoURL} target='_blank' >{chain.infoURL}</MuiLink>}
         avatar={<CustomAvatar {...chain} />}
       />
       <CardContent>
@@ -40,16 +41,6 @@ export const ChainCard: React.FC<Chain> = (chain: Chain) => {
           container
           rowSpacing={2}
         >
-          <Grid xs={12} justifyContent='space-around'>
-            <Stack alignItems='center'>
-              <Typography color="text.secondary">
-                Website:
-              </Typography>
-              <Typography color="text.primary">
-                <MuiLink href={chain.infoURL} target='_blank' >{chain.infoURL}</MuiLink>
-              </Typography>
-            </Stack>
-          </Grid>
           <Grid xs={6}>
             <Stack alignItems='center'>
               <Typography color="text.secondary">
@@ -95,18 +86,14 @@ export const ChainCard: React.FC<Chain> = (chain: Chain) => {
         </Stack> */}
       </CardContent >
       <CardActions sx={{ width: '100%' }}>
-        <Stack
-          direction='row'
+        <Button
+          fullWidth
+          size='small'
+          component={Link}
+          to={createRoute[Route.Chain](chain.chainId)}
         >
-          <Button
-            fullWidth
-            size='small'
-            component={Link}
-            to={createRoute[Route.Chain](chain.chainId)}
-          >
-            More
-          </Button>
-        </Stack>
+          More
+        </Button>
       </CardActions>
     </Card >
   )
